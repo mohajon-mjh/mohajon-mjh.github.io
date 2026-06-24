8119997f96fa7dc4816731e67d70848bbd949eb8
Mohajon MJH <mohajonmjh@gmail.com>
2026-06-24 16:47:12 +0300

    Auto Update: 2026-06-24 16:47:12

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE='mjh-v1';
+const ASSETS=[
+'/',
+'/index.html',
+'/products.html',
+'/product-details.html',
+'/cart.html',
+'/checkout.html',
+'/assets/images/logo.png',
+'/manifest.json'
+];
+
+self.addEventListener('install',e=>{
+e.waitUntil(
+caches.open(CACHE).then(c=>c.addAll(ASSETS))
+);
+self.skipWaiting();
+});
+
+self.addEventListener('activate',e=>{
+e.waitUntil(
+caches.keys().then(keys=>Promise.all(
+keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+))
+);
+self.clients.claim();
+});
+
+self.addEventListener('fetch',e=>{
+e.respondWith(
+caches.match(e.request).then(r=>r||fetch(e.request))
+);
+});
