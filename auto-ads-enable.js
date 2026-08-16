4e7e681392bcd7cf0220f727a10e9d7b64c1f0b2
Mohajon MJH <mohajonmjh@gmail.com>
2026-08-16 21:30:56 +0300

    Enable Auto Ads: anchor + vignette (highest CPM)

---
+++ auto-ads-enable.js
@@ -0,0 +1,13 @@
+(function(){
+ var PUB="ca-pub-5496507853221471";
+ function enable(){
+  (window.adsbygoogle=window.adsbygoogle||[]).push({
+   google_ad_client:PUB,
+   enable_page_level_ads:true,
+   overlays:{bottom:true},
+   vignette:{enable:true}
+  });
+ }
+ if(document.readyState==="complete")enable();
+ else window.addEventListener("load",enable);
+})();
