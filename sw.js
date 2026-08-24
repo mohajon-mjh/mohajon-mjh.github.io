8b5ec447304621f839309dd2f83347ecd43ad00d
Mohajon MJH <mohajonmjh@gmail.com>
2026-08-24 12:34:45 +0300

    Bust service worker cache - force fresh home-products.html on all devices

--- sw.js
+++ sw.js
@@ -1,12 +1,3 @@
-/* mjh-sw-v6 : no cache */
 self.addEventListener("install",function(e){self.skipWaiting();});
-self.addEventListener("activate",function(e){
- e.waitUntil((async function(){
-  var ks=await caches.keys();
-  for(var i=0;i<ks.length;i++){await caches.delete(ks[i]);}
-  await self.clients.claim();
- })());
-});
-self.addEventListener("fetch",function(e){
- e.respondWith(fetch(e.request));
-});
+self.addEventListener("activate",function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.map(function(k){return caches.delete(k);}));}).then(function(){return clients.claim();}));});
+self.addEventListener("fetch",function(e){});
