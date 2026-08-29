0e545742e8f7be4631f9ec6106ee26054379be03
Mohajon MJH <mohajonmjh@gmail.com>
2026-08-29 22:17:06 +0300

    Product media: real images + videos for 9 products (auto media-map)

---
+++ assets/products/media-map.js
@@ -0,0 +1,11 @@
+window.MJH_MEDIA={
+"huggies-dry-pant-diaper-large-50pcs":{main:"main.jpg",images:"img1.jpg img2.jpg img3.jpg img4.jpg img5.jpg img6.jpg img7.jpg img8.jpg img9.jpg img10.jpg img11.jpg ",videos:"video1.mp4 "},
+"inline-roller-skates":{main:"main.jpg",images:"img1.jpg img2.jpg img3.jpg img4.jpg img5.jpg img6.jpg img7.jpg img8.jpg img9.jpg img10.jpg img11.jpg img12.jpg img13.jpg ",videos:"video1.mp4 "},
+"kemei-km-3365-professional-hair-dryer-1800w":{main:"main.jpg",images:"img1.jpg img2.jpg img3.jpg img4.jpg img5.jpg img6.jpg img7.jpg img8.jpg img9.jpg img10.jpg img11.jpg img12.jpg img13.jpg img14.jpg img15.jpg ",videos:"video1.mp4 "},
+"kemei-km-5017-rechargeable-hair-beard-clipper":{main:"main.jpg",images:"img1.jpg img2.jpg img3.jpg img4.jpg ",videos:""},
+"philips-bt1230-beard-trimmer-series-1000":{main:"main.jpg",images:"img1.jpg img2.jpg img3.jpg img4.jpg ",videos:""},
+"philips-bt3125-beard-trimmer-series-3000":{main:"main.png",images:"img1.jpg img2.jpg img3.png ",videos:""},
+"philips-bt3302-beard-trimmer-series-3000":{main:"main.png",images:"img1.jpg img2.jpg img3.jpg img4.jpg img5.jpg img6.jpg img7.jpg img8.jpg img9.jpg img10.jpg img11.jpg img12.jpg img13.jpg img14.jpg img15.png ",videos:"video1.mp4 video2.mp4 video3.mp4 "},
+"samsung-galaxy-a04e":{main:"main.png",images:"img1.jpg img2.jpg img3.jpg img4.jpg img5.jpg img6.jpg img7.jpg img8.jpg img9.png ",videos:"video1.mp4 "},
+"vigor-v-908-fast-hair-straightener":{main:"main.jpg",images:"img1.jpg img2.jpg img3.jpg img4.jpg img5.jpg img6.jpg img7.jpg img8.jpg ",videos:""},
+};
